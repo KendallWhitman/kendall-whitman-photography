25a624fc88bc8460f4fdadcf302bf256f384a0f1
Kendall Whitman <kendallw@protonmail.com>
2022-05-16 23:42:55 -0500

    remove home folder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import { graphql } from 'gatsby'
-import HomePage from './home'
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import { Layout } from '../components';
 import '../styles/index.scss'
 
-const IndexPage = ({ data }) => (
-  <HomePage data={data} />
-)
+const IndexPage = ({ data }) => {
+  const [index, setIndex] = useState(0)
+  const imageArr = data?.allFile?.edges
+  const endOfImages = index < imageArr?.length - 1
+  
+  useEffect(() => {
+    setTimeout(() => setIndex(endOfImages ? index + 1 : 0), 5000);
+  }, [index]);
+
+  return (
+    <Layout>
+      <GatsbyImage
+        alt=''
+        className='slideshow'
+        image={getImage(imageArr[index].node)}
+      />
+    </Layout>
+  )
+}
 
 export default IndexPage;
 
