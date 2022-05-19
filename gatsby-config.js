d932379385537924ccecbbf3111a67e92ea640cb
Kendall Whitman <kendallw@protonmail.com>
2022-05-18 22:00:37 -0500

    master: add old analytics id

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,7 +73,8 @@ module.exports = {
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          "G-D7GMKX04DH"
+          "G-D7GMKX04DH",
+          "UA-120756713-2",
         ],
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
