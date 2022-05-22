696845c7dc270ec932d06a3ce151d8746105ea0b
Kendall Whitman <kendallw@protonmail.com>
2022-05-22 11:15:00 -0500

    master: update analytics plugin config

--- gatsby-config.js
+++ gatsby-config.js
@@ -71,28 +71,18 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
           "G-D7GMKX04DH",
           "UA-120756713-2",
         ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
         gtagConfig: {
-          // optimize_id: "OPT_CONTAINER_ID",
           anonymize_ip: true,
           cookie_expires: 0,
+          send_page_view: true,
         },
-        // This object is used for configuration specific to this plugin
         pluginConfig: {
-          // Puts tracking script in the head instead of the body
-          head: false,
-          // Setting this parameter is also optional
+          head: true,
           respectDNT: true,
-          // Avoids sending pageview hits from custom paths
-          exclude: [],
-          // Defaults to https://www.googletagmanager.com
-          // origin: "YOUR_SELF_HOSTED_ORIGIN",
         },
       },
     },
