263161c030fce26096bae486243eab9ed62d9880
Kendall Whitman <kendallw@protonmail.com>
2022-05-24 17:01:59 -0500

    master: fix google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,52 +68,18 @@ module.exports = {
         },
       },
     },
-    // {
-    //   resolve: `gatsby-plugin-google-gtag`,
-    //   options: {
-    //     trackingIds: [
-    //       // "G-D7GMKX04DH",
-    //       //"UA-120756713-2",
-    //     ],
-    //     gtagConfig: {
-    //       anonymize_ip: true,
-    //       cookie_expires: 0,
-    //     },
-    //     pluginConfig: {
-    //       head: true,
-    //       respectDNT: true,
-    //     },
-    //   },
-    // },
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        // The property ID; the tracking code won't be generated without it
-        trackingId: "UA-120756713-2",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: false,
-        // Avoids sending pageview hits from custom paths
-        exclude: [],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        // Enables Google Optimize using your container Id
-        // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
-        // Enables Google Optimize Experiment ID
-        // experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
-        // Set Variation ID. 0 for original 1,2,3....
-        // variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
-        // Defers execution of google analytics script after page load
-        defer: false,
-        // Any additional optional fields
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: "kendallwhitman.photography",
-        // defaults to false
-        enableWebVitalsTracking: true,
+        trackingIds: ["UA-120756713-2"],
+        gtagConfig: {
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        pluginConfig: {
+          head: false,
+          respectDNT: true,
+        },
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
